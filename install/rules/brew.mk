c3d88585f8f128c95b39a6b23aca064d381ad018
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2024-08-19 17:36:36 +0200

    Make the homebrew installation checks more explicit

--- install/rules/brew.mk
+++ install/rules/brew.mk
@@ -6,10 +6,12 @@
 brew: brew-install brew-setup
 
 brew-install:
-	echo "$(BREW_LOG) install homebrew if not present..."
-	test $$(command -v $(HOMEBREW_BIN_PATH)/brew) \
-		&& echo "$(BREW_LOG) brew already installed!" \
-		|| bash -c "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	if ! command -v $(HOMEBREW_BIN_PATH)/brew >/dev/null; then \
+		echo "$(BREW_LOG) installing homebrew on your system..."; \
+		/bin/bash -c "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; \
+	else \
+		echo "$(BREW_LOG) brew already installed!"; \
+	fi
 	eval "$$($(HOMEBREW_BIN_PATH)/brew shellenv)"
 
 brew-setup: brew-install
