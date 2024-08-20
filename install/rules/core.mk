b95fc89a5eb4b8f38ab66729aea785604ea41fa1
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2024-08-20 19:27:00 +0200

    Simplify ssh key presence check

--- install/rules/core.mk
+++ install/rules/core.mk
@@ -8,7 +8,8 @@ core: shell ssh
 shell: make-default-user-shell sh-symlink
 
 # make the updated version of zsh the default shell when I open a new terminal.
-# NOTE: homebrew version of zsh must be added to the list of recognised shells, otherwise the chsh command would not allow the update 
+# NOTE: homebrew version of zsh must be added to the list of recognised shells,
+# otherwise the chsh command would not allow the update 
 make-default-user-shell: zsh
 	echo "$(CORE_LOG) change default shell to zsh..."
 	if ! grep -Fxq $(HOMEBREW_BIN_PATH)/zsh $(SYS_SHELLS); then \
@@ -19,18 +20,21 @@ make-default-user-shell: zsh
 	fi
 
 # update sh symlink to points to zsh instead of bash 
-# NOTE: could not symlink to /opt/homebrew/bin/zsh, linked it to the preinstalled /bin/zsh instead
+# NOTE: could not symlink to /opt/homebrew/bin/zsh,
+# linked it to the preinstalled /bin/zsh instead
 sh-symlink: zsh
 	echo "$(CORE_LOG) update sh symlink from bash to zsh..."
 	if ! sh --version | grep -q zsh; then \
 		sudo ln -sfv /bin/zsh /var/select/sh; \
 	fi
 
+# NOTE: it checks the .ssh/ dir presence, under the assumption
+# that it won't be created prior to the ssh-keys copy operation. 
 ssh: ansible
 	echo "$(CORE_LOG) setup ssh keys..."
-	if [ ! -e $$(${HOME}/.ssh/{personal,work}) ]; then \
-		mkdir -p ${HOME}/.ssh; \
-		cp $(SSH_KEYS)/* ${HOME}/.ssh; \
+	if [ ! -d ${HOME}/.ssh ]; then \
+		mkdir -p ${HOME}/.ssh;
+		cp $(SSH_KEYS)/* ${HOME}/.ssh && \
 		ansible-vault decrypt ${HOME}/.ssh/personal ${HOME}/.ssh/work; \
 	else \
 		echo "$(CORE_LOG) ssh keys already present!"; \
