2d7502e892e9eb8484d94775e86b18559dfea255
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2024-08-21 15:38:08 +0200

    Remove the cleanup step (remove tmp files within the steps they are used in)

--- install/rules/dev.mk
+++ install/rules/dev.mk
@@ -1,9 +1,9 @@
-.PHONY: dev setup-node personal work github-login gitlab-login mk-tmp-dir cleanup
+.PHONY: dev setup-node personal work github-login gitlab-login mk-tmp-dir
 
 .DEFAULT_GOAL := dev
 
 
-dev: setup-node personal work cleanup
+dev: setup-node personal work
 
 # setup fnm and corepack install dirs, and install lts and latest version of node
 # NOTE: at the moment, node versions are: v22.2.0 (latest), and v20.13.1 (LTS) 
@@ -34,6 +34,7 @@ personal: github-login
 			echo "$(DEV_LOG) $$repo is already present, skipped!"; \
 		fi; \
 	done < ${XDG_DATA_HOME}/personal-repo
+	rm ${XDG_DATA_HOME}/personal-repo
 
 # clone work projects
 work: gitlab-login
@@ -52,6 +53,7 @@ work: gitlab-login
 			echo "$(DEV_LOG) $$repo is already present, skipped!"; \
 		fi; \
 	done < ${XDG_DATA_HOME}/work-repo
+	rm ${XDG_DATA_HOME}/work-repo
 
 # login to gh via access token
 github-login: mk-tmp-dir
@@ -62,6 +64,9 @@ github-login: mk-tmp-dir
 		cat ${XDG_DATA_HOME}/gh-login-token | pbcopy && \
 		echo "$(DEV_LOG) token successfully copeid on your clipboard! Paste it during the installation process..." && \
 		gh auth login; \
+		rm ${XDG_DATA_HOME}/gh-login-token \
+	else \
+		echo "$(DEV_LOG) already logged in into github"; \
 	fi
 
 # login to glab via access token
@@ -73,15 +78,10 @@ gitlab-login: mk-tmp-dir
 		cat ${XDG_DATA_HOME}/glab-login-token | pbcopy && \
 		echo "$(DEV_LOG) token successfully copeid on your clipboard! Paste it during the installation process..." && \
 		glab auth login; \
+		rm ${XDG_DATA_HOME}/glab-login-token; \
+	else \
+		echo "$(DEV_LOG) already logged in into gitlab"; \
 	fi
 
 mk-tmp-dir: 
 	mkdir -p $(XDG_DATA_HOME)
-
-# remove temporary files
-cleanup:
-	echo "$(DEV_LOG) Cleaning up your system from the following temporary files: "
-	echo "- ${XDG_DATA_HOME}/personal-repo" && rm ${XDG_DATA_HOME}/personal-repo
-	echo "- ${XDG_DATA_HOME}/work-repo" && rm ${XDG_DATA_HOME}/work-repo
-	echo "- ${XDG_DATA_HOME}/gh-login-token" && rm ${XDG_DATA_HOME}/gh-login-token
-	echo "- ${XDG_DATA_HOME}/glab-login-token" && rm ${XDG_DATA_HOME}/glab-login-token
