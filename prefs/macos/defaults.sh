d3a51a3d21154a85fc66f7357fce95a01a790df9
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2024-08-24 19:29:30 +0200

    Integrate 'System Settings > General > Language & Region' default prefs

--- prefs/macos/defaults.sh
+++ prefs/macos/defaults.sh
@@ -12,3 +12,40 @@ sudo scutil --set ComputerName $REPLY
 sudo scutil --set HostName $REPLY
 sudo scutil --set LocalHostName $REPLY
 defaults write com.apple.rapport familySyncedName -string $REPLY
+
+# Allow text selection in images
+defaults write -g AppleLiveTextEnabled -bool true
+
+
+#------------------------------------------------------------------------------------#
+#                      • LANG, LOCALE, FORMATS, UNITS •                              #
+#------------------------------------------------------------------------------------#
+
+# Set preferred languages
+defaults write -g AppleLanguages -array "en-US" "it-IT"
+
+# Set locale to IT keeping the EN language
+# drive formats for date/time, number, currencies, etc
+defaults write -g AppleLocale -string "en_US@rg=itzzzz"
+defaults write -g AKLastLocale -string "en_US@rg=itzzzz"
+defaults write com.apple.dock region -string "IT"
+defaults write com.apple.iCal NotificationsLastLocale -string "en_US@rg=itzzzz"
+defaults write com.apple.iCal BirthdayEventsGenerationLastLocale -string "en_US@rg=itzzzz"
+
+# Switch to Fahrenheit as temperature unit (leave unset for Celsius)
+# defaults write -g AppleTemperatureUnit -string "Fahrenheit"
+
+# Leave unset to maintain Monday as first day of the week
+# defaults delete -g AppleFirstWeekday
+
+# Leave unset to maintain the metric system for units
+# NOTE: possible values are:
+# ----------------------------------------------------------------
+# system            AppleMetricUnits        AppleMeasurementUnits
+# ----------------------------------------------------------------
+# Metric            leave unset             leave unset
+# Imperial (US)     0                       "Inches"
+# Imperial (UK)     1                       "Inches"
+# ----------------------------------------------------------------
+# defaults write -g AppleMetricUnits -int 0
+# defaults write -g AppleMeasurementUnits -int "Inches"
