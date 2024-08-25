8ffab7dd244e7e58fc8947fa8c410e000d739181
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2024-08-25 16:20:52 +0200

    Integrate 'System Settings > Siri & Spotlight' default prefs

--- prefs/macos/defaults.sh
+++ prefs/macos/defaults.sh
@@ -172,3 +172,48 @@ for item in \
   defaults write com.apple.controlcenter "NSStatusItem Visible $item" -bool false &>/dev/null
   defaults delete com.apple.controlcenter "NSStatusItem Preferred Position $item" &>/dev/null
 done
+
+
+#------------------------------------------------------------------------------------#
+#                                 • SPOTLIGHT •                                      #
+#------------------------------------------------------------------------------------#
+
+# disable spotlight indexing for all volumes
+sudo mdutil -a -i off
+
+# Change indexing order and disable some search results
+defaults write com.apple.spotlight orderedItems -array \
+	'{"enabled" = 1;"name" = "APPLICATIONS";}' \
+	'{"enabled" = 1;"name" = "DIRECTORIES";}' \
+	'{"enabled" = 1;"name" = "PDF";}' \
+	'{"enabled" = 1;"name" = "DOCUMENTS";}' \
+	'{"enabled" = 1;"name" = "IMAGES";}' \
+	'{"enabled" = 1;"name" = "PRESENTATIONS";}' \
+	'{"enabled" = 1;"name" = "SPREADSHEETS";}' \
+	'{"enabled" = 0;"name" = "SYSTEM_PREFS";}' \
+	'{"enabled" = 0;"name" = "FONTS";}' \
+	'{"enabled" = 0;"name" = "MESSAGES";}' \
+	'{"enabled" = 0;"name" = "CONTACT";}' \
+	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
+	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
+	'{"enabled" = 0;"name" = "MUSIC";}' \
+	'{"enabled" = 0;"name" = "MOVIES";}' \
+	'{"enabled" = 0;"name" = "SOURCE";}'
+
+# Load new settings before rebuilding the index, ...
+killall mds > /dev/null 2>&1
+# ...make sure indexing is enabled for the main volume
+sudo mdutil -i on / > /dev/null
+# ...erase and rebuild the index from scratch
+sudo mdutil -E / > /dev/null
+
+
+#------------------------------------------------------------------------------------#
+#                                   • SIRI •                                         #
+#------------------------------------------------------------------------------------#
+
+# disable Siri
+defaults write com.apple.assistant.support Assistant\ Enabled -bool false
+defaults write com.apple.Siri StatusMenuVisible -bobl false
+defaults write com.apple.Siri VoiceTriggerUserEnabled -bobl false
+defaults write com.apple.Siri SiriPrefStashedStatusMenuVisible -bobl false
