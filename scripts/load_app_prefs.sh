78ae1d2016f790115bae485b4b79746ec2498a58
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-01-11 12:41:41 +0100

    Silence defaults command errors on not existing domain

--- scripts/load_app_prefs.sh
+++ scripts/load_app_prefs.sh
@@ -9,7 +9,10 @@ local APP_PREFS="$HOME/.dotfiles/prefs/apps"
 
 local _load_settings() {
   _log "$LOG_PREFIX loading preferences for $1 app..."
-  source $APP_PREFS/$1-settings.sh
+  # silence any 'Domain XXX not found' errors (prefs get set anyway)
+  # NOTE: the error is only thrown when using defaults via this script,
+  # not when executing directly the commands on the shell prompt.
+  source $APP_PREFS/$1-settings.sh 2> /dev/null
 }
 
 _load_settings alt-tab
