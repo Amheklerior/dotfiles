30c3c1376826448d3cb33cfff3d7a45ba94a7ba5
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-02-14 18:48:43 +0100

    simplify node env setup by installing only the LTS version of node out by default

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -7,28 +7,16 @@ local LOG_PREFIX="[setup dev env]:"
 
 _log "$LOG_PREFIX setting up node dev env..."
 
-local _install_node() {
-  v=$1
-  if [[ ! $(fnm list | grep -q $v) ]]; then
-    _log "$LOG_PREFIX installing node $v versions"
-    fnm install --$v
-  else
-    _log "$LOG_PREFIX node $v version already installed on this system"
-  fi
-}
-
 # create node and corepack install dirs
 mkdir -p $HOME/.fnm $HOME/.corepack && \
 _log "$LOG_PREFIX created node and corepack install dirs: ~/.fnm, ~/.corepack"
 
-# install LTS and latest versions of node
-_install_node lts
-_install_node latest
-
-# installing pnpm as package manager of choice
-# NOTE. I need to get latest version of corepack because trying to install pnpm with the version
-# of corepack that comes with node causes signature validation errors -> https://pnpm.io/it/installation#using-corepack
-npm i -g corepack@latest
-corepack enable pnpm
+# install LTS node version
+if [[ ! $(fnm list | grep -q lts) ]]; then
+  _log "$LOG_PREFIX installing node LTS version..."
+  fnm install --lts
+else
+  _log "$LOG_PREFIX node LTS version already installed on this system"
+fi
 
 _log "$LOG_PREFIX node dev env setup complete!"
