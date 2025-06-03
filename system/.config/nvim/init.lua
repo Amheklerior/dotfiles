21fb4658ca0971d3c4b85af0a4f3aeb107f4edea
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-06-03 10:15:08 +0200

    add some quick-testing utilities

--- system/.config/nvim/init.lua
+++ system/.config/nvim/init.lua
@@ -1,1 +1,13 @@
 print "hello baby"
+
+--[[ useful keymaps to avoid restarting neovim only to test lua code ]]
+
+-- execute the current file
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+
+-- execute the current line
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+
+-- execute the selected code
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
