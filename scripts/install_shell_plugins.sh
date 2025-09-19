50472e0e343e0eeb3adf8ff37e32be5ce0284c7a
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-09-19 13:15:59 +0200

    simplify shell plugins installation step

--- scripts/install_shell_plugins.sh
+++ scripts/install_shell_plugins.sh
@@ -2,23 +2,16 @@
 
 local LOG_PREFIX="[install shell plugins]:"
 
-local PLUGINS_LIST="$DOTFILES_REPO/bundles/shell-plugins.list"
-
 # load utility functions in case the script is run by the user
 [[ -o interactive ]] && source "$DOTFILES_REPO/scripts/utils.sh"
 
 # create the shell plugins dir if it doesn't exist
 mkdir -p $XDG_CONFIG_HOME/plugins
 
-# read the plugins list and install them
-while IFS=' ' read -r plugin repo; do
-  if [[ ! -e "$XDG_CONFIG_HOME/plugins/$plugin" ]]; then
-    _log "$LOG_PREFIX installing $plugin from $repo..."
-    gh repo clone $repo $XDG_CONFIG_HOME/plugins/$plugin \
-      || _log "$LOG_PREFIX Failed to clone: $plugin"
-  else
-    _log "$LOG_PREFIX $plugin is already installed, skipped!"
-  fi
-done < $PLUGINS_LIST
+# install shell plugins
+if [ ! -e "$XDG_CONFIG_HOME/plugins/fzf-tab" ]; then
+  _log "$LOG_PREfiX installing the fzf-tab shell plugin..."
+  git clone https://github.com/Aloxaf/fzf-tab.git $XDG_CONFIG_HOME/plugins/fzf-tab
+fi
 
 _log "$LOG_PREFIX shell plugins installation complete!"
